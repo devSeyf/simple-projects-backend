55fd7549e087b7ab9dfbf0f8b1d774839f790c6a
devSeyf <ssaaiiffjjaammaall@gmail.com>
2026-01-04 22:59:21 +0300

    tride run the app :(

---
+++ Nodejs_quick_53_assignments/app.js
@@ -0,0 +1,7 @@
+// Assignment 1
+// Question 1: Read contents of a file named 'data.txt' and log it.
+// Answer 1:
+
+const fs = require('Assignment 1/data.txt');
+// Read file asynchronously with callback
+fs.readFile('data.txt', 'utf8');
