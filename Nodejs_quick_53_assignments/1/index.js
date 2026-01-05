d69d1608047b21b08af492c7cc562deadd43fe2c
devSeyf <ssaaiiffjjaammaall@gmail.com>
2026-01-06 00:42:26 +0300

    added problem number one

--- Nodejs_quick_53_assignments/1/index.js
+++ Nodejs_quick_53_assignments/1/index.js
@@ -2,6 +2,7 @@
 // Question 1: Read contents of a file named 'data.txt' and log it.
 // Answer 1:
 
+const fs = require("fs");
 
-
-console.log("hi saif ");
+const data = fs.readFileSync("data.txt", "utf8");
+console.log(data);
