7dd00d1562f6de7595757f0977987b1e2afe777c
devSeyf <ssaaiiffjjaammaall@gmail.com>
2026-01-06 00:48:39 +0300

    all problem in one file

--- Nodejs_quick_53_assignments/1/index.js
+++ Nodejs_quick_53_assignments/1/index.js
@@ -1,8 +1,16 @@
 // Assignment 1
 // Question 1: Read contents of a file named 'data.txt' and log it.
 // Answer 1:
-
+/*
 const fs = require("fs");
-
 const data = fs.readFileSync("data.txt", "utf8");
 console.log(data);
+ */
+
+// ================================================= 
+// Assignment 2
+// Question 2: Create a CLI tool to count lines in a file passed as argument.
+// Answer 2:
+
+
+ 
